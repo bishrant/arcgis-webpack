994cc1b47f7de3983c86632dda4902765b3d3498
Bishrant Adhikariu <bishrant.adhikari@tfs.tamu.edu>
2021-05-06 10:29:54 -0500

    Added pdf merging library

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,37 @@ import MapView from '@arcgis/core/views/MapView';
 import Map from '@arcgis/core/Map';
 import './index.css';
 
+import { PDFDocument } from 'pdf-lib';
+
+
+
+async function mergePDFDocuments(documents: any) {
+	const mergedPdf = await PDFDocument.create();
+
+	for (let document of documents) {
+	
+
+		const copiedPages = await mergedPdf.copyPages(document, document.getPageIndices());
+		copiedPages.forEach((page) => mergedPdf.addPage(page));    
+	}
+	
+	return await mergedPdf.save();
+}
+
+const MergeAndDownloadPDF = async () => {
+    const arrayBuffer = await fetch('./assets/sample.pdf').then(res => res.arrayBuffer())
+    const pdfDoc = await PDFDocument.load(arrayBuffer);
+    const arrayBuffer2 = await fetch('/assets/sample2.pdf').then(res => res.arrayBuffer())
+    const pdfDoc2 = await PDFDocument.load(arrayBuffer2);
+
+    const pbyte = await mergePDFDocuments([pdfDoc, pdfDoc2]);
+    var blob = new Blob([pbyte], {type: "application/pdf"});
+    var link = document.createElement("a");
+    link.href = window.URL.createObjectURL(blob);
+    link.download = "myFileName.pdf";
+    link.click();
+};
+
 const initMap = async () => {
     const esriMap = new Map({
         basemap: 'streets'
@@ -20,4 +51,6 @@ const initMap = async () => {
 
 window.addEventListener('DOMContentLoaded', (event) => {
     initMap();
+    const btn = document.querySelector("#mergePDF");
+    btn.addEventListener('click', () => MergeAndDownloadPDF());
 });
