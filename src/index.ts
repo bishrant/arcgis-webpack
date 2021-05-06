d83fac0475545ed25b0e0371595025641f321864
Bishrant Adhikariu <bishrant.adhikari@tfs.tamu.edu>
2021-05-06 10:32:37 -0500

    Isolated pdf load function

--- src/index.ts
+++ src/index.ts
@@ -4,14 +4,15 @@ import './index.css';
 
 import { PDFDocument } from 'pdf-lib';
 
-
+const LoadPDFFile = async (name: string) => {
+    const arrayBuffer = await fetch(`assets/${name}`).then(res => res.arrayBuffer())
+    const pdfDoc = await PDFDocument.load(arrayBuffer);
+    return pdfDoc;
+}
 
 async function mergePDFDocuments(documents: any) {
 	const mergedPdf = await PDFDocument.create();
-
 	for (let document of documents) {
-	
-
 		const copiedPages = await mergedPdf.copyPages(document, document.getPageIndices());
 		copiedPages.forEach((page) => mergedPdf.addPage(page));    
 	}
@@ -20,11 +21,8 @@ async function mergePDFDocuments(documents: any) {
 }
 
 const MergeAndDownloadPDF = async () => {
-    const arrayBuffer = await fetch('./assets/sample.pdf').then(res => res.arrayBuffer())
-    const pdfDoc = await PDFDocument.load(arrayBuffer);
-    const arrayBuffer2 = await fetch('/assets/sample2.pdf').then(res => res.arrayBuffer())
-    const pdfDoc2 = await PDFDocument.load(arrayBuffer2);
-
+    const pdfDoc = await LoadPDFFile('sample.pdf');
+    const pdfDoc2 = await LoadPDFFile('sample2.pdf');
     const pbyte = await mergePDFDocuments([pdfDoc, pdfDoc2]);
     var blob = new Blob([pbyte], {type: "application/pdf"});
     var link = document.createElement("a");
